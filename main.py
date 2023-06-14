982c72b646a4e22bbb24a113e53173f5f460d0c1
AshenGrove <tombreslin31@gmail.com>
2023-06-14 22:03:17 +0100

    More boilerplate

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from discord import app_commands
 from discord.ext import commands
 import json
 
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+        
